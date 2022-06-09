7ee400b64de963c54bc5790a5f62c4a5e5b3b6ae
loai abed alhi <loai007thbk@hotmail.com>
2022-06-09 13:36:26 +0300

    routes and exe 1 done

--- src/App.js
+++ src/App.js
@@ -1,35 +1,80 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Fentities from "./components/Fentities";
+import "./App.css";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       wizards: [
-        { name: "Merlin", power: "Wisdom", other: "Helped King Arthur", imgUrl: "https://tinyurl.com/merlin-image" },
-        { name: "Morgana Le Fay", power: "Forces of Nature", other: "Trapped Merlin in a cave for eternity", imgUrl: "https://tinyurl.com/morgana-image" },
-        { name: "Gandalf", power: "Plot Convenience", other: "Once broke a bridge", imgUrl: "https://tinyurl.com/gandalf-img" }
+        {
+          name: "Merlin",
+          power: "Wisdom",
+          other: "Helped King Arthur",
+          imgUrl: "https://tinyurl.com/merlin-image",
+        },
+        {
+          name: "Morgana Le Fay",
+          power: "Forces of Nature",
+          other: "Trapped Merlin in a cave for eternity",
+          imgUrl: "https://tinyurl.com/morgana-image",
+        },
+        {
+          name: "Gandalf",
+          power: "Plot Convenience",
+          other: "Once broke a bridge",
+          imgUrl: "https://tinyurl.com/gandalf-img",
+        },
       ],
       bestiary: [
-        { name: "Smaug", power: "Fire and Flying", other: "Burned a city to with his mouth", imgUrl: "https://tinyurl.com/smaug-image" },
-        { name: "Buckbeak", power: "Flying", other: "Knocked over Malfoy like a boss", imgUrl: "https://tinyurl.com/buckbeak-image" },
-        { name: "Cerebrus", power: "Having three heads", other: "Holding back the dead since 100 BCE", imgUrl: "https://tinyurl.com/cerebrus-image" }
-      ]
-    }
+        {
+          name: "Smaug",
+          power: "Fire and Flying",
+          other: "Burned a city to with his mouth",
+          imgUrl: "https://tinyurl.com/smaug-image",
+        },
+        {
+          name: "Buckbeak",
+          power: "Flying",
+          other: "Knocked over Malfoy like a boss",
+          imgUrl: "https://tinyurl.com/buckbeak-image",
+        },
+        {
+          name: "Cerebrus",
+          power: "Having three heads",
+          other: "Holding back the dead since 100 BCE",
+          imgUrl: "https://tinyurl.com/cerebrus-image",
+        },
+      ],
+    };
   }
-  
-  render() {
-    const state = this.state
-    return (
-      <div className="App">
-        <div id="home-background"></div>
-        <div id="main-links">
-          {/* Main Links */}
-        </div>
-        {/* Routes go here v */}
 
-        {/* Routes go here ^ */}
-      </div>
+  render() {
+    const state = this.state;
+    return (
+      <Router>
+        <div className="App">
+          <div id="home-background"></div>
+          <div id="main-links">{/* Main Links */}</div>
+          <Link to="/">Home </Link>
+          <Link to="/About">About</Link>
+
+          <Route path="/" exact component={Home} />
+          <Route
+            path="/about"
+            exact
+            render={() => <About items={Object.keys(state)} />}
+          />
+          <Route
+            path="/directory/:fentities"
+            exact
+            render={({ match }) => <Fentities match={match} state={state} />}
+          />
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Fentities.js
+++ src/components/Fentities.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-import '../styles/fentity-directory.css'
+import React, { Component } from "react";
+
+import "../styles/fentity-directory.css";
 
 class Fentities extends Component {
-    render() {
-        return (
-            <div>
-                <h1 id="fentities-title">{/*Get from `match`*/}</h1>
-                <div id="fentities-container">
-                    {fentities.map(f => {
-                        return (
-                            <div className="mini">
-                                <img className="directory-img" src={f.imgUrl} alt="" />
-                                <span>{f.name}</span>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>)
-    }
+  render() {
+    const fentitiesCategory = this.props.match.params.fentities;
+    const fentities = this.props.state[fentitiesCategory];
+    console.log(fentities);
+    return (
+      <div>
+        <h1 id="fentities-title">{fentitiesCategory}</h1>
+        <div id="fentities-container">
+          {fentities.map((f) => {
+            return (
+              <div className="mini">
+                <img className="directory-img" src={f.imgUrl} alt="" />
+                <span>{f.name}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Fentities
+export default Fentities;
